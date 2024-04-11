59e6cd7a822291d8a98e85a44f68d04b0a4a8102
Louis <louisprotano@hotmail.com>
2024-04-11 16:47:20 +0100

    some functionality

--- calculator.js
+++ calculator.js
@@ -1,1 +1,58 @@
 console.log("calculator connected");
+
+clicker = document.getElementById("calculate");
+
+const date = new Date();
+currentD = date.getDate();
+currentM = date.getMonth()+1;
+currentY = date.getFullYear();
+
+const jan = 31;
+const feb = [28, 29];
+const mar = 31;
+const apr = 30;
+const may = 31;
+const jun = 30;
+const jul = 31;
+const aug = 31;
+const sep = 30;
+const oct = 31;
+const nov = 30;
+const dec = 31;
+
+
+
+clicker.addEventListener("click",() => {
+    inputD = document.getElementById("day").value;
+    inputM = document.getElementById("month").value;
+    inputY = document.getElementById("year").value;
+
+    console.log(`${inputD}, ${inputM}, ${inputY}`);
+
+    if (inputD > 31){
+        document.getElementById("day").classList.add("error");
+        console.log("day error");
+    }
+    else{
+        document.getElementById("day").classList.remove("error");
+    }
+
+    if (inputM > 12){
+        document.getElementById("month").classList.add("error");
+        console.log("month error");
+    }
+    else{
+        document.getElementById("month").classList.remove("error");
+    }
+
+    if (inputY > currentY){
+        document.getElementById("year").classList.add("error");
+        console.log("year error");
+    }
+    else{
+        document.getElementById("year").classList.remove("error");
+    }
+})
+
+console.log(`Date: ${currentD}, ${currentM}, ${currentY}`);
+
